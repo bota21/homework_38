bf830b0174cb6b0db27e6e537a967eabaf412e14
botagoz <botagoz.aimkulova@gmail.com>
2020-11-07 22:12:08 +0600

    #1 Добавлен второй блок страницы и к нему медиазапросы. Также добавлен плавный переход на второй блок

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(() => {
+  $("a.header_wrappers_wrapper_scroll").on("click", function(e){
+    e.preventDefault();
+    var anchor = $(this).attr('href');
+    $('html, body').stop().animate({
+        scrollTop: $(anchor).offset().top - 0
+    }, 800);
+});
+});
