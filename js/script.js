d56261f138ffa87f947f4a0cc9ed9e1d0a48cd69
botagoz <botagoz.aimkulova@gmail.com>
2020-11-08 14:57:57 +0600

    #1 Добавлен пятый блок страницы, добавлен слайдер и медиазапросы

--- js/script.js
+++ js/script.js
@@ -6,4 +6,32 @@ $(() => {
         scrollTop: $(anchor).offset().top - 0
     }, 800);
 });
+$('.slick').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
 });
+});
+
+{/* <div class="container promo_container">
+<h5 class="promo_wrappers_title">specialties</h5>
+<div class="promo_wrappers">
+  <div class="promo_wrappers_wrapper">
+    <div class="promo_wrappers_wrapper_frame wrapper_frame"></div>
+    <img src="img/desert.png" alt="Desert" class="promo_wrapper_image">
+  </div>
+  <div class="promo_wrappers_wrapper">
+    <h3 class="promo_wrappers_wrapper_title">chocolate pancakes</h3>
+    <div class="promo_wrappers_wrapper_border wrapper_border"></div>
+    <p class="promo_wrappers_wrapper_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis at
+      velit maximus, molestie est a, tempor magna.</p>
+    <p class="promo_wrappers_wrapper_text">Integer ullamcorper neque eu purus euismod, ac faucibus mauris
+      posuere. Morbi non ultrices ligula. Sed dictum, enim sed ullamcorper feugiat, dui odio vehicula eros,
+      a
+      sollicitudin lorem quam nec sem. Mauris tincidunt feugiat diam convallis pharetra. Nulla facilisis
+      semper
+      laoreet.</p>
+  </div>
+</div>
+</div> */}
